0ccba682d5e69d7964e841a1b32e7fc763f029f9
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-03-26 15:14:32 +0100

    Updates

---
+++ precache-manifest.2e06799a66a856a22933f0943efc2936.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1c80d42bdfb33e04b5233121dd3b03",
+    "url": "/gabrieljealmo.com/static/media/BlackJack_Game.dc1c80d4.JPG"
+  },
+  {
+    "revision": "ad16debaba9b2f798ee8",
+    "url": "/gabrieljealmo.com/static/css/main.df907a77.chunk.css"
+  },
+  {
+    "revision": "1c2886350fe95d6814c1",
+    "url": "/gabrieljealmo.com/static/js/runtime~main.a6e66e55.js"
+  },
+  {
+    "revision": "3e9695650bc64bd28618",
+    "url": "/gabrieljealmo.com/static/js/2.4548674f.chunk.js"
+  },
+  {
+    "revision": "2e616202514aff4def77fb18da580697",
+    "url": "/gabrieljealmo.com/static/media/Intro_BG_Filter_1.2e616202.jpg"
+  },
+  {
+    "revision": "bfea6e2ff70be225918c3ac45ca999e7",
+    "url": "/gabrieljealmo.com/static/media/ProfileImg_BlackAndWhite.bfea6e2f.jpg"
+  },
+  {
+    "revision": "1a561d8ab4cee55763e434cceb3abe25",
+    "url": "/gabrieljealmo.com/static/media/WeatherApplication.1a561d8a.JPG"
+  },
+  {
+    "revision": "2a13a2f7600f78089a2bd26799ebbf49",
+    "url": "/gabrieljealmo.com/static/media/CurrencyConverter.2a13a2f7.JPG"
+  },
+  {
+    "revision": "8b46f5c8da766c5c0a4cce077a9b2bba",
+    "url": "/gabrieljealmo.com/static/media/Tomasos_Pizza.8b46f5c8.JPG"
+  },
+  {
+    "revision": "9f316521020b450251993f8719d2c702",
+    "url": "/gabrieljealmo.com/static/media/BlogApplication.9f316521.JPG"
+  },
+  {
+    "revision": "2fd5b0c345a2865c88405dcca71dd856",
+    "url": "/gabrieljealmo.com/static/media/Designpatterns_Application.2fd5b0c3.JPG"
+  },
+  {
+    "revision": "ad16debaba9b2f798ee8",
+    "url": "/gabrieljealmo.com/static/js/main.87b2f440.chunk.js"
+  },
+  {
+    "revision": "9517f4c7c61cde06c99c0bfdf2794718",
+    "url": "/gabrieljealmo.com/static/media/Shotgun_Game.9517f4c7.JPG"
+  },
+  {
+    "revision": "42ea64895a83f9a0a03192f50d0e6749",
+    "url": "/gabrieljealmo.com/static/media/First_Webpage.42ea6489.JPG"
+  },
+  {
+    "revision": "0dd7ffdbee90f76a1ee23a77e3f10a21",
+    "url": "/gabrieljealmo.com/static/media/WinForms_HotelApp.0dd7ffdb.JPG"
+  },
+  {
+    "revision": "e026a5f67f4701848f9e2ceec4cbfd11",
+    "url": "/gabrieljealmo.com/static/media/WinForms_Cookbook.e026a5f6.JPG"
+  },
+  {
+    "revision": "bbb127401c075de361ab637321fc2c97",
+    "url": "/gabrieljealmo.com/static/media/BankApplication.bbb12740.JPG"
+  },
+  {
+    "revision": "927297d8d9eaf7f7eebabe4d371e550b",
+    "url": "/gabrieljealmo.com/static/media/TextAdventure_Game.927297d8.JPG"
+  },
+  {
+    "revision": "d5a1221ce0e5a07b36e682d91bf96020",
+    "url": "/gabrieljealmo.com/static/media/SudokuSolver.d5a1221c.JPG"
+  },
+  {
+    "revision": "ff206957c6f83994458852fdb0df43df",
+    "url": "/gabrieljealmo.com/static/media/Hangman_Game.ff206957.JPG"
+  },
+  {
+    "revision": "3d3fc1fc702f1cc69d00916188488b10",
+    "url": "/gabrieljealmo.com/static/media/GitHub.3d3fc1fc.png"
+  },
+  {
+    "revision": "1a3b508bfda70922ff69eaa6221a42ae",
+    "url": "/gabrieljealmo.com/static/media/ProfileImg.1a3b508b.jpg"
+  },
+  {
+    "revision": "331e1b5f4f243d6368522cbbbc8a74b7",
+    "url": "/gabrieljealmo.com/index.html"
+  }
+];
