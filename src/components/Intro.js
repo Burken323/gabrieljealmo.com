dd9cd7d92fa580e2fb1dc600aba5d0e463cabdff
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-03-27 19:31:52 +0100

    Responsivity edits..

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,6 @@ import React from 'react';
 export class Intro extends React.Component{
     render(){
         const IntroPicture = require('../images/ProfileImg_BlackAndWhite.jpg');
-        console.log(IntroPicture);
         return (
             <div className="introContent">
                 <img className="introPicture" src={IntroPicture} alt="" />
