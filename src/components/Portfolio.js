dd65594cee47a38802ff22fb07bc1051262abe1c
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-03-27 10:11:19 +0100

    Color test..

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -27,65 +27,65 @@ export class Portfolio extends React.Component{
                         <div className="portfolioImages">
                             <a className="portfolioTag" target="_blank" href={one} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={one} alt="" />
-                                <p>Väderapp som hämtar nuvarande samt en forecast på 6 dagar från API.</p>
+                                <p id="tagText">Väderapp som hämtar nuvarande samt en forecast på 6 dagar från API.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={two} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={two} alt="" />
-                                <p>Valutaomvandlare som hämtar nuvarande valutor via API.</p>
+                                <p id="tagText">Valutaomvandlare som hämtar nuvarande valutor via API.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={three} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={three} alt="" />
-                                <p>Webshop för pizzeria, gjord med ASP.NET och identity.</p>
+                                <p id="tagText">Webshop för pizzeria, gjord med ASP.NET och identity.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={four} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={four} alt="" />
-                                <p>Blogg, utvecklad i ASP.NET med databas för posts/comments.</p>
+                                <p id="tagText">Blogg, utvecklad i ASP.NET med databas för posts/comments.</p>
                             </a>
                         </div>
                         <div className="portfolioImages">
                             <a className="portfolioTag" target="_blank" href={five} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={five} alt="" />
-                                <p>'Kamerasystem' utvecklat i ASP.NET med hjälp av 5 olika designpatterns.</p>
+                                <p id="tagText">'Kamerasystem' utvecklat i ASP.NET med hjälp av 5 olika designpatterns.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={six} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={six} alt="" />
-                                <p>Blackjack, utvecklat i Javascript, Html, CSS.</p>
+                                <p id="tagText">Blackjack, utvecklat i Javascript, Html, CSS.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={seven} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={seven} alt="" />
-                                <p>Shotgun, spel som är utvecklat i Javascript, Html och CSS.</p>
+                                <p id="tagText">Shotgun, spel som är utvecklat i Javascript, Html och CSS.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={eight} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={eight} alt="" />
-                                <p>Min första webbsida, enbart CSS, Html.</p>
+                                <p id="tagText">Min första webbsida, enbart CSS, Html.</p>
                             </a>
                         </div>
                         <div className="portfolioImages">
                             <a className="portfolioTag" target="_blank" href={nine} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={nine} alt="" />
-                                <p>Hotelapp utvecklat i Winforms med databas.</p>
+                                <p id="tagText">Hotelapp utvecklat i Winforms med databas.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={ten} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={ten} alt="" />
-                                <p>Kokbok utvecklat i Winforms med databas.</p>
+                                <p id="tagText">Kokbok utvecklat i Winforms med databas.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={eleven} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={eleven} alt="" />
-                                <p>Bankapplikation, utvecklad i C#.</p>
+                                <p id="tagText">Bankapplikation, utvecklad i C#.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={twelwe} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={twelwe} alt="" />
-                                <p>Textbaserat spel, utvecklat i C#.</p>
+                                <p id="tagText">Textbaserat spel, utvecklat i C#.</p>
                             </a>
                         </div>
                         <div className="portfolioImages_Last">
                             <a className="portfolioTag" target="_blank" href={thirteen} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={thirteen} alt="" />
-                                <p>Sudokulösare, utvecklat i C#.</p>
+                                <p id="tagText">Sudokulösare, utvecklat i C#.</p>
                             </a>
                             <a className="portfolioTag" target="_blank" href={fourteen} rel="noopener noreferrer">
                                 <img className="portfolioImage" src={fourteen} alt="" />
-                                <p>Hänga gubben. Utvecklat i C#.</p>
+                                <p id="tagText">Hänga gubben. Utvecklat i C#.</p>
                             </a>
                         </div>
                     </div>
