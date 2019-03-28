424531192d812234f59a0a32ef26d1b5d9de044c
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-03-28 16:11:33 +0100

    Added resume

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export class About extends React.Component{
     render(){
+        const cv = require('../cv/CV_Gabriel_Jealmo.pdf');
         return (
             <div className="aboutContent">
                 <h1 id="aboutLoc">Om Mig</h1>
@@ -11,10 +12,15 @@ export class About extends React.Component{
                             <div className="aboutMeText">
                                 <h2>Hej!</h2>
                                 <p id="tagText">
-                                    Mitt namn är Gabriel Jealmo och jag studerar just nu för att bli Webbutvecklare med fokus på
-                                    .NET CMS.  
+                                    Mitt namn är Gabriel Jealmo och jag studerar just nu på Nackademin för att bli Webbutvecklare 
+                                    med fokus på .NET CMS.  
                                 </p>
                             </div>
+                        </div>
+                        <div className="downloadCV">
+                                <a href={cv} target="_blank" rel="noopener noreferrer">
+                                    <h2>CV</h2>
+                                </a>
                         </div>
                     </div>
                 </div>
