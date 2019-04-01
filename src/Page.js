3250c909eab1b4c0395ec37f6fd41c55c84e0fea
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-04-01 12:39:40 +0200

    Added a portfolioViewer

--- src/Page.js
+++ src/Page.js
@@ -3,21 +3,34 @@ import { Intro } from './components/Intro';
 import { NavBar } from './components/NavBar';
 import { About } from './components/About';
 import { Portfolio } from './components/Portfolio';
-import { Contact } from './components/Contact'
+import { Contact } from './components/Contact';
 
 export default class Page extends React.Component{
     constructor(props){
         super(props);
-        this.state = { position: 0 };
+        this.state = { isShowing: false, img: "" };
+    }
+
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
     }
 
     render(){
+        console.log(this.state.img);
         return (
             <div className="pageContent">
                 <Intro />
                 <NavBar />
                 <About />
-                <Portfolio />
+                <Portfolio isShowing={this.state.isShowing} close={this.closeModalHandler} open={this.openModalHandler} />
                 <Contact />
             </div>
         );
