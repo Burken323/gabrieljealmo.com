d54883a13fee148e42e0c52d8ef1ef6a56e170be
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-04-09 17:11:41 +0200

    Updates

--- precache-manifest.72b27ccec168897d395bdb588eaf3f3c.js
+++ precache-manifest.72b27ccec168897d395bdb588eaf3f3c.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/SudokuSolver.d5a1221c.JPG"
   },
   {
-    "revision": "5039c5bcad1ab4fb1d56",
-    "url": "/static/css/main.7f22f9bb.chunk.css"
+    "revision": "caab36b289ec1aeeef3b",
+    "url": "/static/css/main.39e0cf0b.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "/static/media/TextAdventure_Game.927297d8.JPG"
   },
   {
-    "revision": "5039c5bcad1ab4fb1d56",
+    "revision": "caab36b289ec1aeeef3b",
     "url": "/static/js/main.f728d752.chunk.js"
   },
   {
@@ -152,7 +152,7 @@ self.__precacheManifest = [
     "url": "/static/media/Thumbnail_2.5fd3f3d6.JPG"
   },
   {
-    "revision": "1cf7dfe5e9ed01a7db168e9c3ddd7213",
+    "revision": "3c1c3a694f0ab3ee4b773a1206176ce2",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03cbd5ee424c3cf63f1c7f846e0fa20f.js"
+  "/precache-manifest.72b27ccec168897d395bdb588eaf3f3c.js"
 );
 
 workbox.clientsClaim();
